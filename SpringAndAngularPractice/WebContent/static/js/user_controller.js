1dbf660e8c7a6b37928fa5473e8dc8b5262dbab0
developer-aj <ajju.kvmc@gmail.com>
2016-06-12 00:50:07 +0530

    updating angular controller

--- SpringAndAngularPractice/WebContent/static/js/user_controller.js
+++ SpringAndAngularPractice/WebContent/static/js/user_controller.js
@@ -1,38 +1,87 @@
 'use strict';
 
-App.controller('UserController', ['$scope', 'User', function($scope, User){
-	var self = this;
-	self.user = new User();
-	
-	self.users = [];
-	
-	self.fetchAllUsers = function(){
-		self.users = User.query();
-	}
-	
-	self.createUser = function(){
-		self.user.$save(function(){
+App.controller('UserController', [
+		'$scope',
+		'UserService',
+		function($scope, UserService) {
+			var self = this;
+			self.user = {
+				id : null,
+				username : '',
+				address : '',
+				email : ''
+			};
+			self.users = [];
+
+			self.fetchAllUsers = function() {
+				UserService.fetchAllUsers().then(function(d) {
+					self.users = d;
+				}, function(errResponse) {
+					console.error('Error while fetching users');
+				});
+			}
+
+			self.createUser = function(user) {
+				UserService.createUser(user).then(self.fetchAllUsers,
+						function(errResponse) {
+							console.error('Error while creating user');
+						});
+			};
+
+			self.updateUser = function(user, id) {
+				UserService.updateUser(user, id).then(self.fetchAllUsers,
+						function(errResponse) {
+							console.error('Error while updating user');
+						});
+			};
+
+			self.deleteUser = function(id) {
+				UserService.deleteUser(id).then(self.fetchAllUsers,
+						function(errResponse) {
+							console.error('Error while deleting user');
+						});
+			};
+
 			self.fetchAllUsers();
-		});
-	};
-	
-	self.updateUser = function(){
-		self.user.$update(function(){
-			self.fetchAllUsers();
-		});
-	};
-	
-	self.deleteUser = function(identity){
-		var user = User.get({id:identity}, function(){
-			user.$delete(function(){
-				console.log('Deleting user with id ', identity);
-				self.fetchAllUsers();
-			});
-		});
-	};
-	
-	self.fetchAllUsers();
-	
-	self.edit()
-	
-}])
+
+			self.submit = function() {
+				if (self.user.id === null) {
+					console.log('Saving New User', self.user);
+					self.createUser(self.user);
+				} else {
+					self.updateUser(self.user, self.user.id);
+					console.log('User updated with id ', self.user.id);
+				}
+				self.reset();
+			};
+
+			self.edit = function(id) {
+				console.log('id to be edited', id);
+				for (var i = 0; i < self.users.length; i++) {
+					if (self.users[i].id === id) {
+						self.user = angular.copy(self.users[i]);
+						break;
+					}
+				}
+			};
+
+			self.remove = function(id) {
+				console.log('id to be deleted ', id);
+				if (self.user.id === id) {// clean form if the user to be
+											// deleted is shown there.
+					self.reset();
+				}
+				self.deleteUser(id);
+			};
+
+			self.reset = function() {
+				self.user = {
+					id : null,
+					username : '',
+					address : '',
+					email : ''
+				};
+				$scope.myForm.$setPristine(); //reset Form
+			};
+
+		} ]);
